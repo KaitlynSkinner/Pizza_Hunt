19b08241e463b11b7ccd9ade7ef5da19e0db3a86
Kaitlyn Skinner <kskinnerr@hotmail.com>
2022-05-05 13:08:13 -0400

    controllers and routes added

---
+++ models/Pizza.js
@@ -0,0 +1,27 @@
+// import Schema constructor and model function
+const { Schema, model } = require('mongoose');
+
+// Data to be stored when users create a new pizza (name of pizza, name of user that created pizza, timestamp of when the pizza was created, timestamp of any updates to pizza's data, pizza's suggested size, and pizza's toppings)
+const PizzaSchema = new Schema({
+    pizzaName: { 
+        type: String 
+    },
+    createdBy: { 
+        type: String 
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now
+    },
+    size: {
+        type: String,
+        default: 'Large'
+    },
+    toppings: []
+});
+
+// create the Pizza model using the PizzaSchema
+const Pizza = model('Pizza', PizzaSchema);
+
+// export the Pizza model
+module.exports = Pizza;
